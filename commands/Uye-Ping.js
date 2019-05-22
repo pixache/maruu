0fcee5b9eafecea14a2f309baf9472518240bef2
Pixache <48852379+pixache@users.noreply.github.com>
2019-05-22 18:20:06 +0300

    Update Uye-Ping.js

--- commands/Uye-Ping.js
+++ commands/Uye-Ping.js
@@ -3,7 +3,7 @@ const config = require("../config.json");
 
 module.exports.run = async(client, message, args) => {
 	let msg = await message.channel.send("Gecikme hesaplanıyor...");
-	let embed = new Discord.RichEmbed().setColor(config.yesil).setTitle("Anlık Gecikmen:").setDescription(`:ping_pong: **Anlık Gecikmen:** ${msg.createdTimestamp - message.createdTimestamp}ms.\n:ping_pong: **Bot Gecikmesi:** ${Math.round(client.ping)}ms`)
+	let embed = new Discord.RichEmbed().setColor(config.yesil).setTitle("Ping Pong").setDescription(`:ping_pong: **Anlık Gecikmen:** ${msg.createdTimestamp - message.createdTimestamp}ms.\n:ping_pong: **Bot Gecikmesi:** ${Math.round(client.ping)}ms`)
     msg.edit(embed)
 }
 
