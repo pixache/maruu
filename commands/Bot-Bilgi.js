f081af6fcc370d22d720485831a52e1db949c7be
Pixache <48852379+pixache@users.noreply.github.com>
2019-05-23 17:44:38 +0300

    Update Bot-Bilgi.js

--- commands/Bot-Bilgi.js
+++ commands/Bot-Bilgi.js
@@ -1,27 +1,23 @@
-// Modüller eklendi.
-// Komut mesajları özelleştirildi.
-// Bot-Bilgi özelleştirildi.
-
 const Discord = require("discord.js");
 const config = require("../config.json");
 
 module.exports.run = async(client, message, args) => {
     let totalSeconds = (client.uptime / 1000);
     let hours = Math.floor(totalSeconds / 3600);
-    totalSeconds %= 3600;
+    //totalSeconds %= 3600;
     let minutes = Math.floor(totalSeconds / 60);
-    let uptime = `${hours} saat ${minutes} dakika`;
+    let uptime = `${hours} saat, ${minutes} dakika`;
 
     let bilgi = new Discord.RichEmbed()
         .setColor(config.mavi)
         .setTitle("Maruu > Bilgiler")
         .setThumbnail(client.user.avatarURL)
         .addField(`Destek`, `**Bot Kodlayıcısı:** Xuance#1586\n**Destek Sunucusu:** [Katılmak için tıkla](https://discord.gg/NBA8wYT)`, true)
-        .addField(`Bot Bilgileri`, `**Sürüm:** 0.2.1.1\n**Gecikme:** ${Math.floor(client.ping)} milisaniye\n**Online süresi:** ${uptime}`, true)
-        .addField(`Sunucu Bilgileri`, `**Sunucular:** ${client.guilds.size}\n**Kanallar:** ${client.channels.size}\n**Kullanıcılar:** ${client.users.size}`, true)
-        .addField(`Diğer Bilgiler`, `**Kitaplık:** discord.js\n**Discord.js Sürümü:** 11.3.2\n**NodeJS Sürümü:** 10.15.3`, true)
+        .addField(`Bot Bilgileri`, `**Sürüm:** v0.2.1.3\n**Gecikme:** ${client.ping} milisaniye\n**Online süresi:** ${uptime}`, true)
+        .addField(`Sunucu Bilgileri`, `**Sunucular:** ${client.guilds.size}\n**Kanallar:** ${client.users.size}\n**Kullanıcılar:** ${client.channels.size}`, true)
+        .addField(`Diğer Bilgiler`, `**Kitaplık:** discord.js\n**Discord.js Sürümü:** 11.3.2\n**NodeJS Sürümü:** 10.5.0`, true)
         .setTimestamp()
-        .setFooter(`${message.author.username} tarafından istedi.`, client.user.avatarURL);
+        .setFooter(`${message.author.username} tarafından istendi.`, client.user.avatarURL);
     message.channel.send(bilgi);
 }
 
