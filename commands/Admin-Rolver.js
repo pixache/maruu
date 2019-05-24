da44887e5867e786fd53fc8d2f049ded6046b1c5
Pixache <48852379+pixache@users.noreply.github.com>
2019-05-24 14:43:06 +0300

    Update Admin-Rolver.js

--- commands/Admin-Rolver.js
+++ commands/Admin-Rolver.js
@@ -20,11 +20,11 @@ module.exports.run = async(client, message, args) => {
       if(!member) return fastembed(config.kirmizi, "Hata", `Lütfen sunucunun bir üyesini belirtin.`, message);
       if(!rol) return fastembed(config.kirmizi, "Hata", `Lütfen vereceğiniz rolü belirtin.`, message);
       if(!guildrole) return fastembed(config.kirmizi, "Hata", `${rol} adında bir rol bulunamadı.`, message);
-      if(!member.roles.find(role => role.name === `${rol}`)) return fastembed(config.kirmizi, "Hata", `**${member.user.username}**, **${rol}** rolüne zaten sahip.`, message);
+      if(member.roles.find(role => role.name === `${rol}`)) return fastembed(config.kirmizi, "Hata", `**${member.user.username}**, **${rol}** rolüne zaten sahip.`, message);
 
       member.addRole(guildrole).catch(console.error);
       fastembed(config.yesil, "Rol Verildi", `Rol Verildi: ${member}\nVerilen Rol: ${rol}\nRolü Veren: ${message.author.username}`, message);
-    }if(!message.member.hasPermission("MANAGE_ROLES")){
+    }else{
     	fastembed(config.kirmizi, "Hata", ":no_entry: Bu komutu kullanamazsınız.");
     }
 }
