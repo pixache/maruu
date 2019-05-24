f793bb5c292255b11bd6d849d6adba139b4ed263
Pixache <48852379+pixache@users.noreply.github.com>
2019-05-24 14:47:51 +0300

    Create Uye-Gay.js

---
+++ commands/Uye-Gay.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const config = require("../config.json");
+
+function fastembed(color, title, desc, message) {
+  let embed = new Discord.RichEmbed()
+    .setColor(color)
+    .setTitle(title)
+    .setDescription(desc)
+
+  message.delete();
+  message.channel.send(embed);
+}
+
+module.exports.run = async(client, message, args) => {
+    let uye = message.mentions.members.first() || message.author;
+    let oran = Math.floor(Math.random() * 100 + 1);
+    fastembed(config.mavi, "Gay ölçer", `${uye.username} **${oran}%** gay.`);
+}
+
+module.exports.help = {
+  name: 'gay'
+}
