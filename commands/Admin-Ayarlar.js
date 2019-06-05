cdeecb326d49244b12999f034512b31f3c6de666
Pixache <48852379+pixache@users.noreply.github.com>
2019-06-05 21:51:09 +0300

    Update Admin-Ayarlar.js

--- commands/Admin-Ayarlar.js
+++ commands/Admin-Ayarlar.js
@@ -4,18 +4,8 @@ const guildConf = require('../guildConf.json');
 const fs = require('fs');
 
 module.exports.run = async(client, message, args) => {
-    let guild = message.guild;
-    if(!guildConf[guild.id]) {
-        guildConf[guild.id] = {
-            name: guild.name,
-            welcomeChannel: 'hoşgeldin',
-            welcomeRole: 'Üye'
-        }
-        fs.writeFile('../guildConf.json', JSON.stringify(guildConf, null, 2), (err) => {
-            if(err) console.log(err)
-        });
-    }
-    if(message.member.hasPermission('ADMINISTRATOR') || message.author.id === "361059389731373066") {
+
+    if(message.author.id === "361059389731373066") {
         let embed = new Discord.RichEmbed()
             .setColor(config.mavi)
             .setTitle('Maruu > Sunucu Ayarları')
@@ -24,7 +14,7 @@ module.exports.run = async(client, message, args) => {
             .setFooter('Daha detaylı bilgiler eklenecektir.', message.author.avatarURL);
         message.channel.send(embed);
     }else{
-        message.channel.send("Bunun için yetkiniz yok.")
+        return;
     }
 }
 
