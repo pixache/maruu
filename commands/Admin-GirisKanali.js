b869c80e621f97a4cc70f0ee5f39bec4012752a8
Pixache <48852379+pixache@users.noreply.github.com>
2019-06-05 21:52:18 +0300

    Update Admin-GirisKanali.js

--- commands/Admin-GirisKanali.js
+++ commands/Admin-GirisKanali.js
@@ -13,18 +13,8 @@ function fastembed(color, desc) {
 }
 
 module.exports.run = async(client, message, args) => {
-    let guild = message.guild;
-    if(!guildConf[guild.id]) {
-        guildConf[guild.id] = {
-            name: guild.name,
-            welcomeChannel: 'hoşgeldin',
-            welcomeRole: 'Üye'
-        }
-        fs.writeFile('./guildConf.json', JSON.stringify(guildConf, null, 2), (err) => {
-            if(err) console.log(err)
-        });
-    }
-    if(message.member.hasPermission('ADMINISTRATOR') || message.author.id === `361059389731373066`) {
+
+    if(message.author.id === `361059389731373066`) {
         if(!args[0]) return message.channel.send(fastembed(config.kirmizi, `Giriş-Çıkış kanalını belirtmediniz!`));
         if(message.guild.channels.find(x => x.name === args[0])) {
             if(args[0] === guildConf[message.guild.id].welcomeChannel) return message.channel.send(fastembed(config.kirmizi, `Girdiğiniz kanal zaten şu anki giriş-çıkış kanalı.`));
@@ -35,7 +25,7 @@ module.exports.run = async(client, message, args) => {
             message.channel.send(fastembed(config.kirmizi, 'Bu isimde bir sohbet kanalı bulamadım!'));
         }
     }else{
-        message.channel.send(fastembed(config.kirmizi, 'Bunu yapabilmek için **Yönetici** yetkin olmalı!'));
+        return;
     }
 }
 
